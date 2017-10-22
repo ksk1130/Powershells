eb2650e6e0cd277e2ab92f03b0078c500fd15cad
Sasaki Keisuke <ksk.andr@gmail.com>
2017-10-22 10:57:59 +0900

    新規登録

---
+++ ファイルサイズ上位N個を表示する.ps1
@@ -0,0 +1,9 @@
+﻿param(
+[string]$targetDir
+,[int]$maxViewNum=20
+)
+
+Get-ChildItem $targetDir -Recurse -ErrorAction SilentlyContinue |`
+ Where-Object{$_.PSIsContainer -ne $true} |`
+   Sort-Object Length -Descending |`
+     Select-Object FullName,@{label="SIZE(MB)"; expression={"{0:#,0.00}MB" -f ($_.Length / 1MB)}} -First $maxViewNum
